6523ad871477fbc0154ef287cd59d11bdd42f6f3
Siddesh M <siddeshsiddu1299@gmail.com>
2026-08-12 07:37:17 +0530

    docker implementation

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,6 +16,7 @@ app.use(
       const allowedOrigins = [
         "http://localhost:5173",
         "http://localhost:5174",
+        "http://localhost:5175",
       ];
 
       if (!origin || allowedOrigins.includes(origin)) {
